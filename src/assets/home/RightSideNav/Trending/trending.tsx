8a80fd9515b7d488678a203da1e58822bdb3da5a
Iamlovingtree <toilachungquy6789@gmail.com>
2023-11-02 10:29:28 +0700

    update tag list for tag page

--- src/assets/home/RightSideNav/Trending/trending.tsx
+++ src/assets/home/RightSideNav/Trending/trending.tsx
@@ -7,30 +7,30 @@ type TrendingTagProps = {
     // numberOfPost: number;
 }
 
-const formatNumber = (number: number): string => {
-    if (number >= 1000000) {
-        return (number / 1000000).toFixed(1) + 'm';
-    } else if (number >= 100000) {
-        return (number / 1000).toFixed(1) + 'k';
-    } else if (number >= 10000) {
-        return (number / 1000).toFixed(1) + 'k';
-    } else if (number >= 1000) {
-        return (number / 1000).toFixed(1) + 'k';
-    }
-    return number.toString();
-};
+// const formatNumber = (number: number): string => {
+//     if (number >= 1000000) {
+//         return (number / 1000000).toFixed(1) + 'm';
+//     } else if (number >= 100000) {
+//         return (number / 1000).toFixed(1) + 'k';
+//     } else if (number >= 10000) {
+//         return (number / 1000).toFixed(1) + 'k';
+//     } else if (number >= 1000) {
+//         return (number / 1000).toFixed(1) + 'k';
+//     }
+//     return number.toString();
+// };
 
 const TrendingTag: React.FC<TrendingTagProps> = ({ tags }) => {
     return (
-       
-                    <div key={tags.tagId} className="trending-tag">
-                        <a href="#">
-                            <span>#</span>
-                            <span> {tags.tagName}</span>
-                            {/* <p> {formatNumber(numberOfPost)} posts </p> */}
-                            <p> 10K posts </p>
-                        </a>
-                    </div>
+
+        <div key={tags.tagId} className="trending-tag">
+            <a href="#">
+                <span>#</span>
+                <span> {tags.tagName}</span>
+                {/* <p> {formatNumber(numberOfPost)} posts </p> */}
+                <p> 10K posts </p>
+            </a>
+        </div>
 
     );
 };
--- src/assets/home/blog/BlogListLatest/blog-list-latest.tsx
+++ src/assets/home/blog/BlogListLatest/blog-list-latest.tsx
@@ -11,42 +11,18 @@ const BlogListLatest: React.FC<BlogListProps> = ({ uri }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   const [blogs, setBlogs] = useState<Blog[]>([]);
+  
   const fetchBlogData = async () => {
     const response = await blogApi.get(uri, {withCredentials: true})
     console.log(response.data);
     setBlogs(response.data);
   };
+
   useEffect(() => {
     setIsLoading(true);
     fetchBlogData();
     setIsLoading(false);
   }, [uri]);
-
-// IGNORE OK?
-
-// const [blogs, setBlogs] = useState<Blog[]>([]);
-// const fetchBlogData = async () => {
-//     const response = await fetch(
-//       blogApi.toString() + uri,
-//       {method: 'GET', redirect: "follow", credentials: "include"}
-//     ).then((response) => response);
-
-//     if(response.redirected){
-//       document.location = response.url;
-//     }
-
-//     const data = await response.json();
-//     console.log(response);
-//     console.log(data);
-//     setBlogs(data);
-//   }
-//   useEffect(() => {
-//     setIsLoading(true);
-//     fetchBlogData();
-//     setIsLoading(false);
-//   }, [uri]);
-
-//NOTHING HERE
 
   if (isLoading) {
     return (
