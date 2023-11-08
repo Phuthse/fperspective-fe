79b31ac2e79057637b2fa10a7dd277c88fbccb32
WaveInf <ngnphucan@gmail.com>
2023-11-08 17:53:11 +0700

    add search people to SearchPageMain, add SearchPeopleList, SearchTagList to search page

---
+++ src/assets/SearchPage/SearchPageMain/MainPeople/main-people-list.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { loginApi, userApi } from "../../../../config/axios";
+import User from "../../../../model/user";
+import SearchPageMainPeople from "./main-people";
+
+type MainPeopleListProp = {
+  uri: string;
+};
+
+const MainPeopleList: React.FC<MainPeopleListProp> = ({ uri }) => {
+    
+  const [loginUser, setLoginUser] = useState<string>(' ');
+  const fetchLoginData = async () => {
+    const response = await loginApi.get("/currentUser", { withCredentials: true });
+    setLoginUser(response.data.userID);
+  };
+  useEffect(() => {
+    fetchLoginData();
+  }, [loginApi]);
+
+  const [users, setUsers] = useState<User[]>([]);
+  const fetchUserData = async () => {
+    const response = await userApi.get(uri, { withCredentials: true });
+    setUsers(response.data);
+  };
+  useEffect(() => {
+    fetchUserData();
+  }, [uri]);
+
+  return (
+    <>
+      {users
+      .filter((user) => user.userID != loginUser)
+      .map((filteredUser) => {
+        return <SearchPageMainPeople user={filteredUser} />;
+      })}
+    </>
+  );
+};
+
+export default MainPeopleList;
