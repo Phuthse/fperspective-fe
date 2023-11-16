ca72dc6d4e72eeace48227098c07b30310d55682
Iamlovingtree <toilachungquy6789@gmail.com>
2023-11-16 21:05:35 +0700

    add delete user function

--- src/assets/UserBookmark/user-bookmark.tsx
+++ src/assets/UserBookmark/user-bookmark.tsx
@@ -13,7 +13,7 @@ const UserBookmark: React.FC = () => {
             const response = await loginApi.get("/currentUser", { withCredentials: true });
             setLoginUser(response.data);
         } catch {
-            window.location.href = `${import.meta.env.VITE_FRONTEND_URL}`;
+            window.location.href = `${import.meta.env.VITE_FRONTEND_URL}/login`;
         }
     };
     useEffect(() => {
