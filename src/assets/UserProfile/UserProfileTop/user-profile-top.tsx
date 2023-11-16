e74f60dc2fc3035c067647df9e32510aff6c7a71
WaveInf <ngnphucan@gmail.com>
2023-11-16 15:50:24 +0700

    for merging

--- src/assets/UserProfile/UserProfileTop/user-profile-top.tsx
+++ src/assets/UserProfile/UserProfileTop/user-profile-top.tsx
@@ -83,7 +83,7 @@ const UserProfileTop: React.FC<UserProfileTopProp> = ({ userProfile }) => {
                 <path d="M14.846 1.403l3.752 3.753.625-.626A2.653 2.653 0 0015.471.778l-.625.625zm2.029 5.472l-3.752-3.753L1.218 15.028 0 19.998l4.97-1.217L16.875 6.875z" fill="#5C5F62" />
               </svg>
               <span>
-                <span>{userProfile.category.split(' ').map(word => word.charAt(0)).join('')}</span>
+                <span>{userProfile.category}</span>
               </span>
             </span>
 
